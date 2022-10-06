82b5ea72cb5deb517ade300338d0774b44d92951
Duncan-Kiragu <mathenge089@gmail.com>
2022-10-06 23:04:27 +0300

    changed main app.js file - changes in navigation

--- App.js
+++ App.js
@@ -1,186 +1,209 @@
-import CommentScreen from './screens/CommentScreen';
-import DetailScreen from './screens/DetailScreen';
-import NotificationScreen from './screens/NotificationScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import ReportScreen from './screens/ReportScreen';
-import ScaleScreen from './screens/ScaleScreen';
-// import ScaleStackScreen from './screens/ScaleScreen';
-import NPSScale from './screens/Scales/NPSscale';
+// NAVIGATION
+import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from '@react-navigation/native'; 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {Image} from 'react-native' ;
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const ScaleStack = createStackNavigator();
-function ScaleStackScreen({navigation}) {
-    return (
-      <ScaleStack.Navigator screenOptions={{
-        headerShown: false
-      }}>
-       <ScaleStack.Screen name="Scale Grid" component={ScaleScreen} />  
-       <ScaleStack.Screen name="NPS Scale" component={NPSScale} />           
-      </ScaleStack.Navigator>
-     );
-   }
-  
+import { Image } from "react-native";
+// AUTH
+import SigninScreen from "./screens/auth/SigninScreen";
+import SignupScreen from "./screens/auth/SignupScreen";
 
+// MAIN SCREENS
+import CommentScreen from "./screens/mainScreens/CommentScreen";
+import DetailScreen from "./screens/mainScreens/DetailScreen";
+import NotificationScreen from "./screens/mainScreens/NotificationScreen";
+import ProfileScreen from "./screens/mainScreens/ProfileScreen";
+import ReportScreen from "./screens/mainScreens/ReportScreen";
+import ScaleScreen from "./screens/mainScreens/ScaleScreen";
+import NPSScale from "./screens/Scales/NPSscale";
+
+const AuthStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+const ScaleStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
 
+const ScaleStackScreen = () => (
+  <ScaleStack.Navigator>
+    <ScaleStack.Screen
+      name="Scale"
+      component={ScaleScreen}
+      options={{ headerShown: false }}
+    />
+    <ScaleStack.Screen name="NPSScale" component={NPSScale} />
+  </ScaleStack.Navigator>
+);
 
-// Home Screen
+const ProfileStackScreen = () => (
+  <ProfileStack.Navigator>
+    <ProfileStack.Screen name="Profile" component={ProfileScreen} />
+    <ProfileStack.Screen
+      name="Signin"
+      component={SigninScreen}
+      options={{ title: "DOWELL - LOG IN" }}
+    />
+    <ProfileStack.Screen
+      name="Signup"
+      component={SignupScreen}
+      options={{ title: "DOWELL - SIGN UP" }}
+    />
+  </ProfileStack.Navigator>
+);
 
-export default function App({navigation}) {
-  return (
-    <NavigationContainer>
-     <Tab.Navigator
-     screenOptions={{
-      style: {
-        borderTopWidth: 0,
-        elevation: 0,
-        backgroundColor: 'transparent',
-      },
-      headerStyle: { 
-        borderWidth: 0,
-        // below four properties will remove the shadow
-        borderBottomColor: "transparent",
-        shadowColor: 'transparent',
-        borderBottomWidth: 0,
-        elevation: 0,
-        shadowOpacity: 0,
-        headerShadowVisible: false,
-      },
-  
-      
-    }} 
-    initialRouteName="SelectScale"
-     >
-      
-        <Tab.Screen name="Profile" component={ProfileScreen}
-         options={{
-          tabBarShowLabel: false,  // Remove wording on tab
+const AuthStackScreen = () => (
+  <AuthStack.Navigator>
+    <AuthStack.Screen
+      name="Signin"
+      component={SigninScreen}
+      options={{ title: "DOWELL - LOG IN" }}
+    />
+    <AuthStack.Screen
+      name="Signup"
+      component={SignupScreen}
+      options={{ title: "DOWELL - LOG IN" }}
+    />
+    <AuthStack.Screen name="ScaleScreen" component={ScaleScreen} />
+  </AuthStack.Navigator>
+);
+
+export default () => (
+  <NavigationContainer>
+    <Tab.Navigator
+      screenOptions={{
+        style: {
+          borderTopWidth: 0,
+          elevation: 0,
+          backgroundColor: "transparent",
+        },
+        headerStyle: {
+          borderWidth: 0,
+          // below four properties will remove the shadow
+          borderBottomColor: "transparent",
+          shadowColor: "transparent",
+          borderBottomWidth: 0,
+          elevation: 0,
+          shadowOpacity: 0,
+          headerShadowVisible: false,
+        },
+      }}
+      initialRouteName="SelectScale"
+    >
+      <Tab.Screen
+        name="ProfileScreen"
+        component={ProfileStackScreen}
+        options={{
+          tabBarShowLabel: false, // Remove wording on tab
           headerShown: false,
           tabBarVisible: false,
-          tabBarIcon: ({size}) => {
+          tabBarIcon: ({ size }) => {
             return (
               <Image
                 style={{ width: size, height: size }}
                 source={{
-                  uri: 'https://www.linkpicture.com/q/user-account.png',
+                  uri: "https://www.linkpicture.com/q/user-account.png",
                 }}
               />
-              );
-            },
-          }}
-        />
-        <Tab.Screen name="Data Safety and Personal Privacy" component={DetailScreen}
-         options={{
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Data Safety and Personal Privacy"
+        component={DetailScreen}
+        options={{
           headerShown: false,
-          tabBarShowLabel: false,  // Remove wording on tab
-          tabBarIcon: ({size}) => {
+          tabBarShowLabel: false, // Remove wording on tab
+          tabBarIcon: ({ size }) => {
             return (
               <Image
                 style={{ width: size, height: size }}
                 source={{
                   // uri:'../../assets/DOWELL_ICONS/user-account.png',
-                  uri: 'https://www.linkpicture.com/q/security-policy.jpg',
+                  uri: "https://www.linkpicture.com/q/security-policy.jpg",
                 }}
               />
-              );
-            },
-          }}
-        />
-        <Tab.Screen name="Notifications" component={NotificationScreen}
-         options={{
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={NotificationScreen}
+        options={{
           headerShown: false,
-          tabBarShowLabel: false,  // Remove wording on tab
+          tabBarShowLabel: false, // Remove wording on tab
           tabBarVisible: false,
-          tabBarIcon: ({size}) => {
+          tabBarIcon: ({ size }) => {
             return (
               <Image
                 style={{ width: size, height: size }}
                 source={{
-                  uri: 'https://www.linkpicture.com/q/notifications_1.png',
+                  uri: "https://www.linkpicture.com/q/notifications_1.png",
                 }}
               />
-              );
-            },
-          }}
-        />
-        <Tab.Screen name="Reports" component={ReportScreen}
-         options={{
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Reports"
+        component={ReportScreen}
+        options={{
           headerShown: false,
-          tabBarShowLabel: false,  // Remove wording on tab
+          tabBarShowLabel: false, // Remove wording on tab
           tabBarVisible: false,
-          tabBarIcon: ({size}) => {
+          tabBarIcon: ({ size }) => {
             return (
               <Image
                 style={{ width: size, height: size }}
                 source={{
-                  uri: 'https://www.linkpicture.com/q/reports.png',
+                  uri: "https://www.linkpicture.com/q/reports.png",
                 }}
               />
-              );
-            },
-          }}
-        />
-        {/* InitialRouteName */}
+            );
+          },
+        }}
+      />
+      {/* InitialRouteName */}
 
-
-        <Tab.Screen name="SelectScale" component={ScaleStackScreen} 
-         options={{
+      <Tab.Screen
+        name="SelectScale"
+        component={ScaleStackScreen}
+        options={{
           headerShown: false,
-          tabBarShowLabel: false,  // Remove wording on tab
+          tabBarShowLabel: false, // Remove wording on tab
           tabBarVisible: false,
-          tabBarIcon: ({size}) => {
+          tabBarIcon: ({ size }) => {
             return (
               <Image
                 style={{ width: size, height: size }}
                 source={{
-                  uri: 'https://www.linkpicture.com/q/scale.png',
+                  uri: "https://www.linkpicture.com/q/scale.png",
                 }}
               />
-              );
-            },
-          }}
-        />
+            );
+          },
+        }}
+      />
 
-        <Tab.Screen name="NPSScale" component={NPSScale} 
-         options={{
+      <Tab.Screen
+        name="Close"
+        component={CommentScreen}
+        options={{
           headerShown: false,
-           // Remove wording on tab
+          tabBarShowLabel: false, // Remove wording on tab
           tabBarVisible: false,
-          // tabBarIcon: ({size}) => {
-          //   return (
-          //     <Image
-          //       style={{ width: size, height: size }}
-          //       source={{
-          //         uri: 'https://www.linkpicture.com/q/scale.png',
-          //       }}
-          //     />
-          //     );
-          //   },
-          }}
-        />
-
-        <Tab.Screen name='Close' component={CommentScreen} 
-          options={{
-            headerShown: false,
-            tabBarShowLabel: false,  // Remove wording on tab
-            tabBarVisible: false,
-            tabBarIcon: ({size}) => {
-              return (
-                <Image
-                  style={{ width: size, height: size }}
-                  source={{
-                    uri: 'https://www.linkpicture.com/q/logout-button.png',
-                  }}
-                />
-                );
-              },
-            }}
-          />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
-
+          tabBarIcon: ({ size }) => {
+            return (
+              <Image
+                style={{ width: size, height: size }}
+                source={{
+                  uri: "https://www.linkpicture.com/q/logout-button.png",
+                }}
+              />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
