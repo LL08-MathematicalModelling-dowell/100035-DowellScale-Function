c28426a018206c09dc49c2a54597259485c2b738
Ambrose <ambrosetall@gmail.com>
2023-01-11 09:54:19 +0300

    adding nps scale apis

--- nps-task/nps/urls.py
+++ nps-task/nps/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import dowell_scale_admin,dowell_scale1, default_scale, default_scale_admin,brand_product_error
+from .views import dowell_scale_admin,dowell_scale1, default_scale, default_scale_admin,brand_product_error, scale_settings_api_view, single_scale_settings_api_view, single_scale_response_api_view, scale_response_api_view, settings_api_view_create,scale_response_view_submit
 
 
 app_name="nps"
@@ -10,4 +10,12 @@ urlpatterns = [
     path('nps-scale/embed/', brand_product_error, name='error_page'),
     path('nps-scale/default/', default_scale, name='default_page'),
     path('nps-admin/default/', default_scale_admin, name='default_page_admin'),
+
+    # Rest endpoints
+    path('api/nps_settings', settings_api_view_create, name="create_scale_settings_api"),
+    path('api/nps_responses', scale_response_view_submit, name="nps_response_submit_api"),
+    path('api/nps_settings', scale_settings_api_view, name="scale_settings_api"),
+    path('api/nps_responses', scale_response_api_view, name="scale_response_api"),
+    path('api/nps_settings/<str:id>', single_scale_settings_api_view, name="single_scale_settings_api"),
+    path('api/nps_responses/<str:id>', single_scale_response_api_view, name="single_scale_response_api"),
 ]
