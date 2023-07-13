7bf35f19dd6201442ddebbcf9f3244ed8ccec98b
natty4 <natty7kt@gmail.com>
2023-07-13 13:55:16 +0300

    Updated npslite view

--- nps-task/npslite/views.py
+++ nps-task/npslite/views.py
@@ -57,6 +57,7 @@ def settings_api_view_create(request):
             field_add, "nil")
 
         user_json = json.loads(x)
+        print(user_json, '>>>>>>>>>>>>>>>>>>>>>>>>.')
         details = {"scale_id": user_json['inserted_id'], "event_id": event_id, "user": user}
         user_details = dowellconnection("dowell_npslite_scale", "bangalore", "dowell_npslite_scale", "users", "users", "1098", "ABCDE",
             "insert", details, "nil")
@@ -64,6 +65,7 @@ def settings_api_view_create(request):
 
     elif request.method == 'GET':
         response = request.data
+        print(response, '_____________________________')
         if "scale_id" in response:
             scale_id = response['scale_id']
             field_add = {"_id": scale_id}
@@ -74,6 +76,7 @@ def settings_api_view_create(request):
             field_add = {"scale_category": "npslite scale"}
             x = dowellconnection("dowell_npslite_scale", "bangalore", "dowell_npslite_scale", "scale", "scale", "1093", "ABCDE", "fetch",
                 field_add, "nil")
+            print(x, '_____________________________')
             return Response({"data": json.loads(x)})
 
     elif request.method == "PUT":
@@ -158,6 +161,8 @@ def npslite_response_view(request, id=None):
 
 
 
+
+
 def npslite_home_admin(request):
     context = {}
     context["public_url"] = public_url
@@ -211,3 +216,147 @@ def brand_product_error(request):
         context['existing_scales'].append(b)
 
     name = url.replace("'", "")
+    context['template_url'] = f"{public_url}{name}?brand_name=your_brand&product_name=your_product"
+    print("This are the existing scales", context['existing_scales'] )
+    return render(request, 'lite/embed_page.html', context)
+
+def dowell_npslite_scale_settings(request):
+    user = request.session.get('user_name')
+    if user == None:
+        return redirect(f"https://100014.pythonanywhere.com/?redirect_url={public_url}/nps-lite-admin/settings/")
+    # print("+++++++++=>",user)
+    context={}
+    context["public_url"] = public_url
+    if request.method == 'POST':
+        name = request.POST['nameofscale']
+        question = request.POST['questionofscale']
+        orientation = request.POST['orientation']
+        scalecolor = request.POST['scolor']
+        fontcolor = request.POST['fcolor']
+        left = request.POST["left"]
+        center = request.POST["center"]
+        right = request.POST["right"]
+        time = request.POST['time']
+        no_of_scales = request.POST["no_of_scales"]
+        rand_num = random.randrange(1, 10000)
+        template_name = f"{name.replace(' ', '')}{rand_num}"
+        if time == "":
+            time = 0
+        try:
+            eventID=get_event_id()
+            field_add={"eventId":eventID,"settings":{"question":question,"orientation":orientation,"scalecolor":scalecolor,"fontcolor":fontcolor,"time":time,"template_name":template_name,"name":name,"center":center, "left":left,"right":right, "scale-category": "npslite scale", "no_of_scales":no_of_scales}}
+            x = dowellconnection("dowellscale","bangalore","dowellscale","scale","scale","1093","ABCDE","insert",field_add,"nil")
+            print("This is what is saved", x)
+            # User details
+            user_json = json.loads(x)
+            details = {"scale_id": user_json['inserted_id'], "event_id": eventID, "username": user}
+            user_details = dowellconnection("dowellscale", "bangalore", "dowellscale", "users", "users", "1098",
+                "ABCDE", "insert", details, "nil")
+            print("+++++++++++++", user_details)
+            return redirect(f"{public_url}/nps-lite/nps-lite-scale/{template_name}")
+        except:
+            context["Error"] = "Error Occurred while save the custom pl contact admin"
+    return render(request, 'lite/settings_page.html', context)
+
+@xframe_options_exempt
+@csrf_exempt
+def dowell_npslite_scale(request, tname):
+    user = request.session.get('user_name')
+    if user == None:
+        user = "Anonymous"
+
+    print("___+++_+ USER", user)
+    context={}
+    context["public_url"] = public_url
+    brand_name = request.GET.get('brand_name', None)
+    product_name = request.GET.get('product_name', None)
+    ls = request.path
+    url = request.build_absolute_uri()
+    try:
+        x,s = url.split('?')
+        names_values_dict = dict(x.split('=') for x in s.split('&'))
+        xy = x[1].replace('&', ',')
+        y = xy.replace('=', ':')
+        z = '{'+y+'}'
+        # return HttpResponse(names_values_dict['brand_name'])
+        pls = ls.split("/")
+        tname1 = pls[1]
+        # resp = response.objects.all()
+        # return HttpResponse(resp)
+        context["brand_name"] = names_values_dict['brand_name']
+        context["product_name"] = names_values_dict['product_name'].split('/')[0]
+        context["scale_name"] = tname
+    except:
+        f_path = request.get_full_path()
+        response = redirect('nps_lite:display_embed_page')
+        # response.delete_cookie('url')
+        response.set_cookie('url', f_path)
+        return response
+
+    context["url"]="../scaleadmin"
+    context["urltext"]="Create new scale"
+    context["btn"]="btn btn-dark"
+    context["hist"]="Scale History"
+    context["bglight"]="bg-light"
+    context["left"]="border:silver 2px solid; box-shadow:2px 2px 2px 2px rgba(0,0,0,0.3)"
+    # context["npsall"]=system_settings.objects.all().order_by('-id')
+    # scale settings call
+    field_add = {"settings.template_name": tname, }
+    default = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale", "scale", "1093", "ABCDE", "fetch",
+        field_add, "nil")
+    data = json.loads(default)
+
+    context["scale_id"] = data['data'][0]['_id']
+    x = data['data'][0]['settings']
+    context["defaults"] = x
+    number_of_scale = x['no_of_scales']
+    context["no_of_scales"] = number_of_scale
+    current_url = url.split('/')[-1]
+    context['cur_url'] = current_url
+
+    # find existing scale reports
+    field_add = {"scale_data.scale_id": context["scale_id"]}
+    response = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale_reports", "scale_reports", "1094",
+        "ABCDE", "fetch", field_add, "nil")
+    data = json.loads(response)
+
+    existing_scale = False
+    if len(data['data']) != 0 and data['data'][0]['_id'] != '642f1f98b85416d087c3ac57':
+        score_data = data["data"]
+
+        for i in score_data:
+            instance_id = i['category'][0]['instance_id'].split("/")[0]
+            if instance_id == current_url:
+                existing_scale = True
+                context['response_saved'] = i['category'][0]['category']
+                context['score'] = "show"
+        if data["data"][0]["scale_data"]["scale_id"] == "63b7ccac0175aa060a42b554":
+            existing_scale = False
+            # context['response_saved'] = i['score'][0]['score']
+            context['score'] = ""
+
+    if request.method == 'POST':
+        category = request.POST['scoretag']
+        eventID = get_event_id()
+        category = {"instance_id": f"{current_url}/{context['no_of_scales']}", 'category': category}
+        if len(data['data']) != 0:
+            if data["data"][0]["scale_data"]["scale_id"] == "63b7ccac0175aa060a42b554":
+                category = {"instance_id": f"Default", 'category': category}
+
+
+        if existing_scale == False:
+            try:
+                field_add={"event_id":eventID,"scale_data":{"scale_id":context["scale_id"],"scale_type":"nps scale"}, "brand_data":{"brand_name":context["brand_name"],"product_name":context["product_name"]},"category":[category]}
+                x = dowellconnection("dowellscale","bangalore","dowellscale","scale_reports","scale_reports","1094","ABCDE","insert",field_add,"nil")
+                print("Nps Lite Scale Response---->",x)
+                # User details
+                user_json = json.loads(x)
+                details = {"scale_id": user_json['inserted_id'], "event_id": eventID, "username": user}
+                user_details = dowellconnection("dowellscale", "bangalore", "dowellscale", "users", "users", "1098",
+                    "ABCDE", "insert", details, "nil")
+                context['score'] = "show"
+                print("++++++++++", user_details)
+                # return redirect(f"https://100014.pythonanywhere.com/main")
+            except:
+                context["Error"] = "Error Occurred while save the custom pl contact admin"
+    return render(request,'lite/single_scale.html',context)
