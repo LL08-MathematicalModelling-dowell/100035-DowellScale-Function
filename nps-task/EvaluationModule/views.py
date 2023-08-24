50cf9414d1dd03079674034b716ec82223c6f4ae
iamumarjaved <81705788+iamumarjaved@users.noreply.github.com>
2023-08-24 08:59:04 +0500

    New Evluation Module function

--- nps-task/EvaluationModule/views.py
+++ nps-task/EvaluationModule/views.py
@@ -318,3 +318,164 @@ def Target_API(request):
 
     # If the request failed, return an error response
     return JsonResponse({"error": "Failed to retrieve data from the original API."}, status=500)
+
+
+
+@api_view(['POST'])
+def evaluation_api(request):
+    if request.method == 'POST':
+        payload = request.data
+        process_id = payload['process_id']
+        doc_no = payload['doc_no']
+        random_number = generate_random_number()
+        context = {}
+
+        # Fetch data from cache if available
+        cache_key = f"evaluation_editor_{process_id}_{doc_no}"
+        cached_data = cache.get(cache_key)
+        if cached_data:
+            return render(request, 'EvaluationModule/editor_reports.html', cached_data)
+
+        field_add = {"process_id": process_id}
+
+        # Execute dowellconnection API call using ThreadPoolExecutor
+        with ThreadPoolExecutor() as executor:
+            data_future = executor.submit(dowellconnection, "dowellscale", "bangalore", "dowellscale", "scale_reports", "scale_reports",
+                                          "1094", "ABCDE", "fetch", field_add, "nil")
+            data = json.loads(data_future.result())["data"]
+
+        all_scales = [x for x in data if x['score'][0]['instance_id'].split("/")[0] == doc_no]
+        calculate_score = [x['score'][0]['score'] for x in all_scales if x["scale_data"]["scale_type"] == "nps scale"]
+        # print(f"\n\ndata: {calculate_score}\n\n")
+        # print(f"\n\nall_scales: {all_scales}\n\n")
+
+        if len(data) != 0:
+            scores = process_data(data, doc_no)
+            nps_scales = len(scores["nps scale"])
+            nps_score = sum(scores["nps scale"])
+            stapel_scales = len(scores["stapel scale"])
+            stapel_score = scores["stapel scale"]
+
+            context.update({
+                "nps_scales": nps_scales,
+                "nps_score": nps_score,
+                "nps_total_score": nps_scales * 10,
+                "stapel_scales": stapel_scales,
+                "stapel_scores": stapel_score,
+                "score_series": scores["nps scale"]
+            })
+
+        # Execute stattricks_api API call using ThreadPoolExecutor
+        with ThreadPoolExecutor() as executor:
+            response_json_future = executor.submit(stattricks_api, "evaluation_module", random_number, 16, 3, {"list1": calculate_score})
+            response_json = response_json_future.result()
+            context.update(response_json)
+
+        poison_case_results = response_json.get("poison case results", {})
+        normal_case_results = response_json.get("normal case results", {})
+        context.update({
+            "poison_case_results": poison_case_results,
+            "normal_case_results": normal_case_results
+        })
+
+        # Execute Normality_api API call using ThreadPoolExecutor
+        with ThreadPoolExecutor() as executor:
+            normality_future = executor.submit(Normality_api, random_number)
+            normality = normality_future.result()
+            context.update(normality)
+
+        normality_data = normality.get('list1') if normality else None
+        context.update({
+            "n_title": normality.get('title'),
+            "n_process_id": normality.get('process_id'),
+            "n_bins": normality.get('bins'),
+            "n_allowed_error": normality.get('allowed_error'),
+            "n_series_count": normality.get('series_count'),
+            "n_list1": normality_data
+        })
+
+        # Cache the data for future requests
+        cache.set(cache_key, context)
+        print(f"stattricks_api: {response_json}\n")
+        print(f"Normality_api: {normality}")
+
+
+        return JsonResponse(context, status=status.HTTP_200_OK)
+
+
+def evaluation_editor_process_id(request, process_id, doc_no):
+    random_number = generate_random_number()
+    context = {}
+
+    # Fetch data from cache if available
+    cache_key = f"evaluation_editor_{process_id}_{doc_no}"
+    cached_data = cache.get(cache_key)
+    if cached_data:
+        return render(request, 'EvaluationModule/editor_reports.html', cached_data)
+
+    field_add = {"process_id": process_id}
+
+    # Execute dowellconnection API call using ThreadPoolExecutor
+    with ThreadPoolExecutor() as executor:
+        data_future = executor.submit(dowellconnection, "dowellscale", "bangalore", "dowellscale", "scale_reports", "scale_reports",
+                                      "1094", "ABCDE", "fetch", field_add, "nil")
+        data = json.loads(data_future.result())["data"]
+
+    all_scales = [x for x in data if x['score'][0]['instance_id'].split("/")[0] == doc_no]
+    calculate_score = [x['score'][0]['score'] for x in all_scales if x["scale_data"]["scale_type"] == "nps scale"]
+    # print(f"\n\ndata: {calculate_score}\n\n")
+    # print(f"\n\nall_scales: {all_scales}\n\n")
+
+    if len(data) != 0:
+        scores = process_data(data, doc_no)
+        nps_scales = len(scores["nps scale"])
+        nps_score = sum(scores["nps scale"])
+        stapel_scales = len(scores["stapel scale"])
+        stapel_score = scores["stapel scale"]
+
+        context.update({
+            "nps_scales": nps_scales,
+            "nps_score": nps_score,
+            "nps_total_score": nps_scales * 10,
+            "stapel_scales": stapel_scales,
+            "stapel_scores": stapel_score,
+            "score_series": scores["nps scale"]
+        })
+
+    # Execute stattricks_api API call using ThreadPoolExecutor
+    with ThreadPoolExecutor() as executor:
+        response_json_future = executor.submit(stattricks_api, "evaluation_module", random_number, 16, 3, {"list1": calculate_score})
+        response_json = response_json_future.result()
+        context.update(response_json)
+
+    poison_case_results = response_json.get("poison case results", {})
+    normal_case_results = response_json.get("normal case results", {})
+    context.update({
+        "poison_case_results": poison_case_results,
+        "normal_case_results": normal_case_results
+    })
+
+    # Execute Normality_api API call using ThreadPoolExecutor
+    with ThreadPoolExecutor() as executor:
+        normality_future = executor.submit(Normality_api, random_number)
+        normality = normality_future.result()
+        context.update(normality)
+
+    normality_data = normality.get('list1') if normality else None
+    context.update({
+        "n_title": normality.get('title'),
+        "n_process_id": normality.get('process_id'),
+        "n_bins": normality.get('bins'),
+        "n_allowed_error": normality.get('allowed_error'),
+        "n_series_count": normality.get('series_count'),
+        "n_list1": normality_data
+    })
+
+    # Cache the data for future requests
+    cache.set(cache_key, context)
+    print(f"stattricks_api: {response_json}\n")
+    print(f"Normality_api: {normality}")
+
+
+    return render(request, 'EvaluationModule/editor_reports.html', context)
+
