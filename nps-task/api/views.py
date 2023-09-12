b0a2274fd5dacc01ca513fd2449b153ce22ceb98
Ambrose <ambrosetall@gmail.com>
2023-09-12 22:38:13 +0300

    Nps lite corrections, user restriction, multiple user submission

--- nps-task/api/views.py
+++ nps-task/api/views.py
@@ -493,13 +493,6 @@ def find_key_by_emoji(emoji_to_find, emoji_dict):
 
 
 def response_submit_loop(response, scale_id, instance_id, user, score, process_id=None, document_data=None):
-    field_add = {"username": user, "scale_data.scale_id": scale_id}
-    previous_response = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale_reports", "scale_reports", "1094", "ABCDE", "fetch",
-                            field_add, "nil")
-    previous_response = json.loads(previous_response)
-    previous_response = previous_response.get('data')            
-    if len(previous_response) > 0 :
-        return Response({"error": "You have already submitted a response for this scale."}, status=status.HTTP_400_BAD_REQUEST)
     field_add = {"_id": scale_id, "settings.scale-category": "nps scale"}
     default_scale = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale", "scale", "1093", "ABCDE",
                                      "find", field_add, "nil")
