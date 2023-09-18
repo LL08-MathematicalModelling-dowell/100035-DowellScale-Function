1d45effa56cfda6f6675561197ce91836e12c2de
natty4 <natty7kt@gmail.com>
2023-09-18 15:55:04 +0300

    feat: added condition statement that verifies the scale type (ranking)

--- nps-task/api/views.py
+++ nps-task/api/views.py
@@ -14,6 +14,7 @@ import likert.views as likert
 import percent_sum.views as percent_sum
 import percent.views as percent
 import npslite.views as nps_lite
+import ranking.views as ranking
 from nps.dowellconnection import dowellconnection
 from nps.eventID import get_event_id
 from dowellnps_scale_function.settings import public_url
@@ -493,6 +494,13 @@ def find_key_by_emoji(emoji_to_find, emoji_dict):
 
 
 def response_submit_loop(response, scale_id, instance_id, user, score, process_id=None, document_data=None):
+    field_add = {"username": user, "scale_data.scale_id": scale_id}
+    previous_response = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale_reports", "scale_reports", "1094", "ABCDE", "fetch",
+                            field_add, "nil")
+    previous_response = json.loads(previous_response)
+    previous_response = previous_response.get('data')            
+    if len(previous_response) > 0 :
+        return Response({"error": "You have already submitted a response for this scale."}, status=status.HTTP_400_BAD_REQUEST)
     field_add = {"_id": scale_id, "settings.scale-category": "nps scale"}
     default_scale = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale", "scale", "1093", "ABCDE",
                                      "find", field_add, "nil")
@@ -891,6 +899,10 @@ def redirect_view(request):
                         return percent.settings_api_view_create(request)
                     elif "percent" in scaletype and "response" in scale_type:
                         return percent.percent_response_view_submit(request)
+                    elif "ranking" in scaletype and "settings" in scale_type:
+                        return ranking.settings_api_view_create(request)
+                    elif "ranking" in scaletype and "response" in scale_type:
+                        return ranking.response_submit_api_view(request)
                     else:
                         return error_response(request, "Scale will be available soon.", status.HTTP_404_NOT_FOUND)
                 else:
