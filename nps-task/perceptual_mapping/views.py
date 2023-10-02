000f63d36b57b4b460c61c1dc7a83cd4e7242d2d
natty4 <natty7kt@gmail.com>
2023-10-03 00:01:38 +0300

    Create A Response endpoint for perceptual_mapping scale

--- nps-task/perceptual_mapping/views.py
+++ nps-task/perceptual_mapping/views.py
@@ -63,7 +63,7 @@ def settings_api_view_create(request):
         field_add = {"event_id": eventID,
                      "settings": {"item_list": item_list, "scale_color": scale_color, "fontstyle": fontstyle,
                                   "no_of_scales": number_of_scales, "fontcolor": fontcolor,
-                                  "time": time, "name": name, "scale-category": "perceptual mapping", "username": username,
+                                  "time": time, "name": name, "scale_category": "perceptual_mapping scale", "username": username,
                                   "item_count": item_count, "X_left": X_left, "X_right": X_right, "Y_top": Y_top,
                                   "Y_bottom": Y_bottom, "marker_color": marker_color, "center": (0,0), "position": "center",
                                    "marker_type": marker_type, "x_range": x_range, "y_range": y_range,
@@ -171,3 +171,111 @@ def settings_api_view_create(request):
                                 field_add, {"settings" : settings})
         return Response({"success": "Successfully Updated ", "data": settings})
 
+
+@api_view(['POST', 'GET'])
+def response_submit_api_view(request):
+    if request.method == 'GET':
+        try:
+            params = request.GET
+            scale_id = params.get("scale_id")
+            if not scale_id:
+                field_add = {"scale_data.scale_type": "perceptual_mapping scale"}
+                response_data = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale_reports", "scale_reports", "1094",
+                                                 "ABCDE", "fetch", field_add, "nil")
+                return Response({"data": json.loads(response_data)}, status=status.HTTP_200_OK)
+
+            field_add = {"_id": scale_id, "scale_data.scale_type": "perceptual_mapping scale"}          
+            scale = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale_reports",
+                                     "scale_reports",
+                                     "1094", "ABCDE", "fetch", field_add, "nil")
+            settings_json = json.loads(scale)
+            if not settings_json.get('data'):
+                return Response({"error": "scale not found"}, status=status.HTTP_404_NOT_FOUND)
+            settings = settings_json['data']
+            return Response({"success": settings_json})
+        except Exception as e:
+            return Response({"Error": "Invalid fields!", "Exception": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+    elif request.method == 'POST':
+        response = request.data
+        try:
+            username = response['username']
+        except:
+            return Response({"error": "Unauthorized."}, status=status.HTTP_401_UNAUTHORIZED)
+        try:
+            scale_id = response['scale_id']
+            product_name = response['product_name']
+            brand_name = response['brand_name']
+            possisions = response['possisions']
+        except KeyError as error:
+            return Response({"error": f"{error.args[0]} missing or misspelled"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+        #check if scale exists
+        field_add = {"_id": scale_id, "settings.scale_category": "perceptual_mapping scale"}
+        x = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale", "scale", "1093", "ABCDE",
+                            "fetch", field_add, "nil")
+        settings_json = json.loads(x)
+        event_id = get_event_id()
+        if not settings_json.get('data'):
+            return Response({"error": "scale not found"}, status=status.HTTP_404_NOT_FOUND)
+        settings = settings_json['data'][0]['settings']
+        possisions = response['possisions']
+        x_range = settings['x_range']
+        y_range = settings['y_range']
+        x_left = settings['X_left']
+        x_right = settings['X_right']
+        y_top = settings['Y_top']
+        y_bottom = settings['Y_bottom']
+        item_list = settings['item_list']
+        item_count = settings['item_count']
+        
+        if isinstance(possisions, dict) == False:
+            return Response({"error": "possisions must be a dict of item and respective x & y possision"}, status=status.HTTP_400_BAD_REQUEST)
+        if len(possisions) != item_count:
+            return Response({"error": f"possisions count must be equal to item_count"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        #check if all items are present in possisions
+        for item in possisions.keys():
+            if item not in item_list:
+                return Response({"error": f"{item} not in item_list"}, status=status.HTTP_400_BAD_REQUEST)
+            
+        #check if all possitions, and items  are valid
+        for item, possision in possisions.items():
+            if isinstance(possision, list) == False:
+                return Response({"error": f"{item} possision must be a tuple"}, status=status.HTTP_400_BAD_REQUEST)
+            if item not in item_list:
+                return Response({"error": f"{item} not in item_list"}, status=status.HTTP_400_BAD_REQUEST)
+            if possision[0] not in x_range:
+                return Response({"error": f"{item} x possision not in x_range"}, status=status.HTTP_400_BAD_REQUEST)
+            if possision[1] not in y_range:
+                return Response({"error": f"{item} y possision not in y_range"}, status=status.HTTP_400_BAD_REQUEST)
+            if possision[0] < x_left or possision[0] > x_right:
+                return Response({"error": f"{item} x possision not in x_left and x_right"}, status=status.HTTP_400_BAD_REQUEST)
+            if possision[1] < y_bottom or possision[1] > y_top:
+                return Response({"error": f"{item} y possision not in y_bottom and y_top"}, status=status.HTTP_400_BAD_REQUEST)
+            
+    
+        field_add = {"event_id": event_id,
+                     "scale_data": {"scale_id": scale_id, "scale_type": "perceptual_mapping scale"},
+                     "brand_data": {"brand_name": brand_name, "product_name": product_name},
+                     "possisions": possisions,
+                     "date_created": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    }
+        
+        x = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale_reports", "scale_reports", "1094",
+                             "ABCDE", "insert", field_add, "nil")
+        response = json.loads(x)
+        details = {"scale_id": response['inserted_id'], "username": username}
+        user_details = dowellconnection("dowellscale", "bangalore", "dowellscale", "users", "users", "1098",
+                                        "ABCDE",
+                                        "insert", details, "nil")
+        return Response({"success": response['inserted_id'], "data": field_add})
+    
+            
+            
+    
+        
+        
+        
