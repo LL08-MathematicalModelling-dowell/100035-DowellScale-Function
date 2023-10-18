b512b99a8dc85e4046a6f409bc75c01db60555c0
Adebayo Adeoye <adeoyeadebayo18@gmail.com>
2023-10-18 09:27:48 +0100

    Implemented GET and PUT method for thurstone scale settings

--- nps-task/thurstone/views.py
+++ nps-task/thurstone/views.py
@@ -40,7 +40,7 @@ def settings_api_view_create(request):
         except KeyError as error:
             return Response({"error": f"{error.args[0]} missing or mispelt"}, status=status.HTTP_400_BAD_REQUEST)
         if statement_count != len(statements):
-            return Response({"error": "item_count must be equal to length of item_list"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "statement count must be equal to length of statements"}, status=status.HTTP_400_BAD_REQUEST)
         if sorting_order == "random":
             statements = dowellshuffling_function(statements)  
         elif sorting_order == "alphabetical":
@@ -71,3 +71,64 @@ def settings_api_view_create(request):
                                         "ABCDE",
                                         "insert", details, "nil")
         return Response({"success": x, "data": field_add})
+    elif request.method == "GET":
+        try:
+            params = request.GET
+            scale_id = params.get("scale_id")
+            if not scale_id:
+                field_add = {"settings.scale-category": "thurstone"}
+                response_data = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale", "scale", "1093",
+                                                 "ABCDE", "fetch", field_add, "nil")
+                return Response({"data": json.loads(response_data)}, status=status.HTTP_200_OK)
+
+            field_add = {"_id": scale_id, "settings.scale-category": "perceptual mapping"}
+            x = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale", "scale", "1093", "ABCDE",
+                                 "find", field_add, "nil")
+            settings_json = json.loads(x)
+            if not settings_json.get('data'):
+                return Response({"error": "scale not found"}, status=status.HTTP_404_NOT_FOUND)
+            settings = settings_json['data']['settings']
+            return Response({"success": settings_json})
+        except Exception as e:
+            return Response({"Error": "Invalid fields!", "Exception": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'PUT':
+        response = request.data
+        if "scale_id" not in response:
+            return Response({"error": "scale_id missing or mispelt"}, status=status.HTTP_400_BAD_REQUEST)
+        id = response['scale_id']
+        field_add = {"_id": id, }
+        x = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale", "scale", "1093", "ABCDE",
+                            "fetch", field_add, "nil")
+        settings_json = json.loads(x)
+        settings = settings_json['data'][0]['settings']
+        for key in settings.keys():
+            if key in response:
+                settings[key] = response[key]
+        statements = settings.get("statements")
+        sorting_order = settings.get("sorting_order")
+        if ("statements" in response) or ("statement_count" in response):
+            statement_count = response.get("statement_count")
+            statements = response.get("statements")
+            if statements == None or statement_count == None:
+                return Response({"error": "statements and statement_count have to be updated together"}, status=status.HTTP_400_BAD_REQUEST)
+            if statement_count != len(statements):
+                return Response({"error": "statement_count must be equal to length of statements"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if "sorting_order" in response:
+            if sorting_order == "random":
+                statements = dowellshuffling_function(statements)  
+            elif sorting_order == "alphabetical":
+                statements = lambda statements: [string.capitalize() for string in statements]
+                statements = sorted(statements)
+            elif sorting_order == "custom":
+                statements = lambda statements: sorted(statements, key=lambda x: x[0])
+
+        settings["statements"] = statements
+        settings["statement_count"] = statement_count
+        settings["sorting_order"] = sorting_order
+        settings["date_updated"] = datetime.datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S")
+        x = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale", "scale", "1093", "ABCDE", "update",
+                                field_add, {"settings":settings})
+        return Response({"success": "Successfully Updated ", "data": settings})
+
