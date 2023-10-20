c6cfbde0178d008aafa04a8be4bb1b9138b0d16f
creedtech <ayodeleayolaoke4@gmail.com>
2023-10-20 12:25:48 +0100

    fixed error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/LL08-MathematicalModelling-dowell/100035-DowellScale-Function/',
+  // base: '/LL08-MathematicalModelling-dowell/100035-DowellScale-Function/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    base: '/LL08-MathematicalModelling-dowell/100035-DowellScale-Function/', // Make sure this is set correctly
+  },
   plugins: [react()],
-  server:{
-    port:3000,
+  server: {
+    port: 3000,
   },
 });
