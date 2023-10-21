8d317a106781aca608fe9fe9bb10eb5129476eb6
creedtech <ayodeleayolaoke4@gmail.com>
2023-10-21 10:22:53 +0100

    changed to base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/LL08-MathematicalModelling-dowell/100035-DowellScale-Function/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    base: '/LL08-MathematicalModelling-dowell/100035-DowellScale-Function/', // Make sure this is set correctly
-  },
+  base: '/100035-DowellScale-Function/',
+  // build: {
+  //   outDir: 'dist',
+  //   assetsDir: 'assets',
+  //   base: '/LL08-MathematicalModelling-dowell/100035-DowellScale-Function/', // Make sure this is set correctly
+  // },
   plugins: [react()],
   server: {
     port: 3000,
