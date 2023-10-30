72d9b0aa97899d872f03b4c841d56bc32de7e555
creedtech <ayodeleayoolaoke4@gmail.com>
2023-10-30 07:47:01 +0100

    adding login implementation

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,28 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import CreatePCSettings from './pages/scales/CreatePCSettings';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Navbar from './components/Navbar';
 import Fallback from './components/Fallback';
 import CreatePCResponse from './pages/scales/CreatePCResponse';
 import SinglePCScaleSettings from './pages/scales/SinglePCScaleSettings';
 import UpdatePCScaleSettings from './pages/scales/UpdatePCScaleSettings';
-import {
-  RankingScale,
-  CreateScale,
-  AvailableScales,
-} from './pages/scales';
+import { RankingScale, CreateScale, AvailableScales } from './pages/scales';
 import { UpdateRankingScale } from './pages/scales/update';
 import { RankingScaleSettings } from './pages/scales/settings';
 import PairedScale from './pages/scales/PairedScale';
 import PerceptualScale from './pages/scales/PerceptualScale';
 import SinglePerceptualScaleSettings from './pages/scales/SinglePerceptualScaleSettings';
 import CreatePerceptualScaleSettings from './pages/scales/CreatePerceptualScaleSettings';
+import PrivateRoutes from './components/PrivateRoute/PrivateRoute';
 
 function App() {
+  // const hasSessionId = /* Check if the session ID exists */ false;
+
+  // if (!hasSessionId) {
+  //   // Redirect to the login page if there's no session ID
+  //   return <Navigate to="/login" />;
+  // }
   return (
     <>
       <div>
@@ -29,41 +33,53 @@ function App() {
         <Suspense fallback={<Fallback />}>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/available-scales" element={<AvailableScales />} />
-            <Route path="/paired-scale-comparison" element={<PairedScale />} />
-            <Route
-              path="/perceptual-mapping-scale"
-              element={<PerceptualScale />}
-            />
-            <Route path="/:slug" element={<RankingScale />} />
-            <Route path="/create-scale" element={<CreateScale />} />
-            <Route
-              path="/create-paired-scale-settings"
-              element={<CreatePCSettings />}
-            />
-            <Route
-              path="/create-perceptual-scale-settings"
-              element={<CreatePerceptualScaleSettings />}
-            />
-            <Route path="/ranking-scale-settings/:slug" element={<RankingScaleSettings />} />
-            <Route
-              path="/create-scale-response/:id"
-              element={<CreatePCResponse />}
-            />
-            <Route
-              path="/single-scale-settings/:id"
-              element={<SinglePCScaleSettings />}
-            />
-            <Route
-              path="/single-perceptual-scale-settings/:id"
-              element={<SinglePerceptualScaleSettings />}
-            />
-            <Route
-              path="/update-paired-scale-settings/:id"
-              element={<UpdatePCScaleSettings />}
-            />
-            <Route path="/update-ranking-scale/:slug" element={<UpdateRankingScale />} />
+            <Route path="/login" element={<Login />} />
+            <Route element={<PrivateRoutes />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/available-scales" element={<AvailableScales />} />
+              <Route
+                path="/paired-scale-comparison"
+                element={<PairedScale />}
+              />
+              <Route
+                path="/perceptual-mapping-scale"
+                element={<PerceptualScale />}
+              />
+              <Route path="/:slug" element={<RankingScale />} />
+              <Route path="/create-scale" element={<CreateScale />} />
+              <Route
+                path="/create-paired-scale-settings"
+                element={<CreatePCSettings />}
+              />
+              <Route
+                path="/create-perceptual-scale-settings"
+                element={<CreatePerceptualScaleSettings />}
+              />
+              <Route
+                path="/ranking-scale-settings/:slug"
+                element={<RankingScaleSettings />}
+              />
+              <Route
+                path="/create-scale-response/:id"
+                element={<CreatePCResponse />}
+              />
+              <Route
+                path="/single-scale-settings/:id"
+                element={<SinglePCScaleSettings />}
+              />
+              <Route
+                path="/single-perceptual-scale-settings/:id"
+                element={<SinglePerceptualScaleSettings />}
+              />
+              <Route
+                path="/update-paired-scale-settings/:id"
+                element={<UpdatePCScaleSettings />}
+              />
+              <Route
+                path="/update-ranking-scale/:slug"
+                element={<UpdateRankingScale />}
+              />
+            </Route>
           </Routes>
         </Suspense>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
 import { useNavigate } from 'react-router';
+import Cookies from 'universal-cookie';
 const Home = () => {
+  // const cookies = new Cookies();
+  // const sessionId = cookies.get('sessionid');
+  // console.log('====================================');
+  // console.log(sessionId);
+  // console.log('====================================');
+  // const navigate = useNavigate();
+  // if (!sessionId) {
+  //   navigate('/login');
+  // }
   const scaleTypes = [
     {
       name: 'linkert scale',
@@ -30,13 +40,13 @@ const Home = () => {
   const navigateTo = useNavigate();
 
   return (
-    <div className="h-screen flex flex-cols justify-center items-center">
-      <div className="w-6/12 grid grid-cols-2 gap-4">
+    <div className="flex items-center justify-center h-screen flex-cols">
+      <div className="grid w-6/12 grid-cols-2 gap-4">
         {scaleTypes.map((scale) => (
           <button
             onClick={() => navigateTo(`/${scale.slug}`)}
             key={scale.slug}
-            className="w-full bg-primary text-white hover:bg-gray-700/50 py-1 px-2 py-2 my-1 capitalize"
+            className="w-full px-2 py-1 py-2 my-1 text-white capitalize bg-primary hover:bg-gray-700/50"
           >
             {scale.name}
           </button>
