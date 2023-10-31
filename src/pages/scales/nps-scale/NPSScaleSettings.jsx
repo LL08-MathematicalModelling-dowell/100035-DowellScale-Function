e33995a73ad0feade8b882166dfe33364243bc46
igah franklin <igahfranklyn@gmail.com>
2023-10-31 11:56:13 +0100

    nps scale

--- src/pages/scales/nps-scale/NPSScaleSettings.jsx
+++ src/pages/scales/nps-scale/NPSScaleSettings.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { toast } from 'react-toastify';
 import { useParams, useNavigate } from "react-router-dom";
 import useGetSingleScale from "../../../hooks/useGetSingleScale";
+import { useSaveResponse } from "../../../hooks/useSaveResponse";
 import Fallback from "../../../components/Fallback";
 import { Button } from "../../../components/button";
 
@@ -9,6 +10,8 @@ const NPSScaleSettings = () => {
     const { slug } = useParams();
     const { loading, sigleScaleData, fetchSingleScaleData } = useGetSingleScale();
     const [selectedScore, setSelectedScore] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const saveResponse = useSaveResponse();
     const navigateTo = useNavigate();
 
     const scores = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -19,6 +22,62 @@ const NPSScaleSettings = () => {
 
     const handleFetchSingleScale = async(scaleId)=>{
       await fetchSingleScaleData(scaleId);
+  }
+
+
+  const submitResponse = async()=>{
+
+    const payload = {
+        document_responses: [
+            {
+                scale_id:slug,
+                score:10
+            },
+            {
+                scale_id:"64f6fc2c7ab91b2af12c3958",
+                score:10
+            }],
+        instance_id:1,
+        brand_name:"question",
+        product_name:"answer",
+        username: "tall",
+        action: "document",
+        authorized: "Ambrose",
+        cluster: "Documents",
+        collection: "CloneReports",
+        command: "update",
+        database: "Documentation",
+        document: "CloneReports",
+        document_flag: "processing",
+        document_right: "add_edit",
+        field: "document_name",
+        function_ID: "ABCDE",
+        metadata_id: "64f568426bcc87ef0c75d43c",
+        process_id: "64f5683c3270cf0e74824fe7",
+        role: "single step role",
+        team_member_ID: "1212001",
+        content: "",
+        document_name: "name",
+        page: "",
+        user_type: "public",
+        _id: slug
+    }
+
+    try {
+        setIsLoading(false);
+        const response = await saveResponse(payload);
+        console.log(response)
+        // if(status===200){
+        //     toast.success('successfully updated');
+        //     setTimeout(()=>{
+        //         navigateTo(`/nps-scale/${sigleScaleData[0]?._id}`);
+        //     },2000)
+        //   }
+    } catch (error) {
+        console.log(error);
+    }finally{
+        setIsLoading(false);
+    }
   }
 
   useEffect(() => {
@@ -56,11 +115,15 @@ const NPSScaleSettings = () => {
             
                     <div className="flex gap-3 justify-end">
                         {sigleScaleData && sigleScaleData.map((scale, index)=>(
-                            <>
-                                <Button width={'3/4'} onClick={()=>navigateTo(`/update-nps-scale/${scale._id}`)} key={index}>update scale</Button>
-                            </>
+                            <Button width={'3/4'} onClick={()=>navigateTo(`/update-nps-scale/${scale._id}`)} key={index}>update scale</Button>
                         ))}
-                        <Button width={'3/4'} primary>Save Response</Button>
+                        <Button 
+                            onClick={submitResponse}
+                            width={'3/4'} 
+                            primary
+                        >   
+                            {isLoading ? 'Saving Response' : 'Save Response'}
+                        </Button>
                     </div>
                 </div>
             </div>
