75c87578ced4b357239e47ec4b464965f9e4145d
creedtech <ayodeleayoolaoke4@gmail.com>
2023-11-28 10:52:03 +0100

    removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
@@ -7,15 +6,11 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter
-      basename={
-        import.meta.env.DEV ? '' : '/100035-DowellScale-Function/'
-      }
-    >
-      <DndProvider backend={HTML5Backend}>
-        <App />
-      </DndProvider>
-    </HashRouter>
-  </React.StrictMode>
+  <HashRouter
+    basename={import.meta.env.DEV ? '' : '/100035-DowellScale-Function/'}
+  >
+    <DndProvider backend={HTML5Backend}>
+      <App />
+    </DndProvider>
+  </HashRouter>
 );
