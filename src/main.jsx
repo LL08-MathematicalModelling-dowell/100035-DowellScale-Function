8f52c433a829f3470510a5dfeabd8aa44334b9ed
creedtech <ayodeleayoolaoke4@gmail.com>
2023-11-28 10:13:08 +0100

    fixed 404 issues by using hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import { DndProvider } from 'react-dnd';
@@ -8,7 +8,7 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter
+    <HashRouter
       basename={
         import.meta.env.DEV ? '' : '/100035-DowellScale-Function/'
       }
@@ -16,6 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <DndProvider backend={HTML5Backend}>
         <App />
       </DndProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
