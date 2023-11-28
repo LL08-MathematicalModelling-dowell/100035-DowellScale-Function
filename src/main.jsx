84d99026fde8153dde9175f6bd5c9db66873b98d
creedtech <ayodeleayoolaoke4@gmail.com>
2023-11-28 14:32:00 +0100

    fixing

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter
-    basename={import.meta.env.DEV ? '' : '/100035-DowellScale-Function/'}
-  >
-    <DndProvider backend={HTML5Backend}>
-      <App />
-    </DndProvider>
-  </HashRouter>
+    // <BrowserRouter>
+      <HashRouter
+        // basename={import.meta.env.DEV ? '' : '/100035-DowellScale-Function/'}
+        basename={window.location.pathname || ''}
+      >
+        <DndProvider backend={HTML5Backend}>
+          <App />
+        </DndProvider>
+      </HashRouter>
+    // </BrowserRouter>
 );
