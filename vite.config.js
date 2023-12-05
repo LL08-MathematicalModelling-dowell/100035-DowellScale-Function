40fd3061eb4a0d5f7edc513e0500156ac765ac4d
Githaiga <pythongeeko@gmail.com>
2023-12-05 10:27:36 +0300

    vite config.js

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ import react from '@vitejs/plugin-react';
 // })
 
 export default defineConfig({
-  base: "/100035-DowellScale-Function",
+  base: "/100035-DowellScale-Function/",
   plugins: [react()],
   server: {
     port: 3000,
