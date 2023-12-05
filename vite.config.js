15b825aaa10a143af73c61f54a3b3a8a0be3d54f
Githaiga <pythongeeko@gmail.com>
2023-12-05 10:19:27 +0300

    vitr config.js

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,26 @@ import react from '@vitejs/plugin-react';
 
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/100035-DowellScale-Function',
-    server: {
-      port: 3000,
-    },
-  }
+// export default defineConfig(({ command }) => {
+//   const config = {
+//     plugins: [react()],
+//     base: '/100035-DowellScale-Function',
+//     server: {
+//       port: 3000,
+//     },
+//   }
 
-  if (command !== 'serve') {
-    config.base = '/100035-DowellScale-Function'
-  }
+//   if (command !== 'serve') {
+//     config.base = '/100035-DowellScale-Function'
+//   }
 
-  return config
+//   return config
+// })
+
+export default defineConfig({
+  base: "/100035-DowellScale-Functior",
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
 })
