82519e913df3094140092020abbc9c69fe07d474
iamumarjaved <81705788+iamumarjaved@users.noreply.github.com>
2023-12-19 04:28:06 +0500

    changes

--- nps-task/EvaluationModule/views.py
+++ nps-task/EvaluationModule/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.core.cache import cache
-
+import string
 from api.views import find_category
 from .calculate_function import *
 from .normality import *
@@ -94,148 +94,6 @@ def evaluation_editor(request, product_name, doc_no):
     print(f"Normality_api: {normality}")
 
     return render(request, 'EvaluationModule/editor_reports.html', context)
-
-
-def csv_new(request, product_name, doc_no):
-    data_list = []
-    headers = {}
-
-    # Fetch data from cache if available
-    cache_key = f"evaluation_editor_{product_name}_{doc_no}"
-    cached_data = cache.get(cache_key)
-    if cached_data:
-        return render(request, 'EvaluationModule/editor_reports.html', cached_data)
-
-    field_add = {"brand_data.product_name": product_name}
-
-    # Execute dowellconnection API call using ThreadPoolExecutor
-    with ThreadPoolExecutor() as executor:
-        data_future = executor.submit(dowellconnection, "dowellscale", "bangalore", "dowellscale", "scale_reports",
-                                      "scale_reports",
-                                      "1094", "ABCDE", "fetch", field_add, "nil")
-        data = json.loads(data_future.result())["data"]
-        # print(f"\n\n data: {data}\n\n")
-
-    all_scales = [x for x in data if x['score'][0]['instance_id'].split("/")[0] == doc_no]
-    calculate_score = [x['score'][0]['score'] for x in all_scales if x["scale_data"]["scale_type"] == "nps scale"]
-    # print(f"\n\ndata: {calculate_score}\n\n")
-    # print(f"\n\nall_scales: {all_scales}\n\n")
-
-    for item in all_scales:
-        data_ = {
-            "scale_id": item['scale_data']['scale_id'],
-            "event_id": item['event_id'],
-            "score": item['score'][0]['score'],
-            "scale_type": item['scale_data']['scale_type'],
-            "product_name": item['brand_data']['product_name']
-        }
-        if data_ not in data_list:
-            data_list.append(data_)
-
-    if len(data) != 0:
-        scores = process_data(data, doc_no)
-        nps_scales = len(scores["nps scale"])
-        nps_score = sum(scores["nps scale"])
-        stapel_scales = len(scores["stapel scale"])
-        stapel_score = scores["stapel scale"]
-        print(f"\n\nnps_scales: {nps_scales}\n\n")
-        print(f"\nscores: {scores}")
-        print(f"\n nps score: {nps_score}")
-
-        headers = {
-            "nps_scales": nps_scales,
-            "nps_score": nps_score,
-            "nps_total_score": nps_scales * 10,
-            "stapel_scales": stapel_scales,
-            "stapel_scores": stapel_score,
-            "score_series": scores["nps scale"]
-        }
-
-    return render(request, 'EvaluationModule/csv_new.html', {"headers": headers, "data_list": data_list})
-
-
-def by_username(request, username, scale_category):
-    global now
-    if scale_category == 'nps':
-        scale_category = 'nps scale'
-    elif scale_category == 'stapel':
-        scale_category = 'stapel scale'
-    elif scale_category == 'npslite':
-        scale_category = 'npslite scale'
-    user_details = dowellconnection("dowellscale", "bangalore", "dowellscale", "users", "users", "1098",
-                                    "ABCDE", "fetch", {"username": username}, "nil")
-    user_dets = json.loads(user_details)
-    scale_i = [entry['scale_id'] for entry in user_dets['data']]
-    list_of_scales = []
-    for scale_id in scale_i:
-        field_add = {"_id": scale_id}
-
-        x = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale", "scale", "1093", "ABCDE",
-                             "fetch", field_add, "nil")
-        settings_json = json.loads(x)
-        data = settings_json['data']
-        if len(data) != 0:
-            now = data[0]
-        else:
-            pass
-
-        try:
-            list_of_scales.append(now['settings']['scale'])
-
-        except KeyError:
-            try:
-                list_of_scales.append(now['scale'])
-            except:
-                try:
-                    list_of_scales.append(now['settings']['scales'])
-                except:
-                    pass
-    print(list_of_scales)
-
-    return render(request, 'EvaluationModule/by_username.html', {"responses": list_of_scales})
-
-
-@api_view(['GET'])
-def by_username_api(request, username, scale_category):
-    global now
-    if scale_category == 'nps':
-        scale_category = 'nps scale'
-    elif scale_category == 'stapel':
-        scale_category = 'stapel scale'
-    elif scale_category == 'npslite':
-        scale_category = 'npslite scale'
-    user_details = dowellconnection("dowellscale", "bangalore", "dowellscale", "users", "users", "1098",
-                                    "ABCDE", "fetch", {"username": username}, "nil")
-    user_dets = json.loads(user_details)
-    # print(user_dets)
-    scale_i = [entry['scale_id'] for entry in user_dets['data']]
-    list_of_scales = []
-    for scale_id in scale_i:
-        field_add = {"_id": scale_id}
-
-        x = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale", "scale", "1093", "ABCDE",
-                             "fetch", field_add, "nil")
-        settings_json = json.loads(x)
-        print(settings_json)
-        data = settings_json['data']
-        if len(data) != 0:
-            now = data[0]
-        else:
-            pass
-
-        try:
-            list_of_scales.append(now['settings']['scale'])
-
-        except KeyError:
-            try:
-                list_of_scales.append(now['scale'])
-            except:
-                try:
-                    list_of_scales.append(now['settings']['scales'])
-                except:
-                    pass
-
-    return Response({"responses": list_of_scales}, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
@@ -583,26 +441,28 @@ def get_brand_product(request):
 
 
 @api_view(['POST', 'GET'])
-def get_scale_report(request):
-    data = request.data
-    scale_id = data.get("scale_id")
-    field_add = {"_id": scale_id}
-    with ThreadPoolExecutor() as executor:
-        data_future = executor.submit(
-            dowellconnection,
-            "dowellscale",
-            "bangalore",
-            "dowellscale",
-            "scale_reports",
-            "scale_reports",
-            "1094",
-            "ABCDE",
-            "fetch",
-            field_add,
-            "nil"
-        )
+def get_scale_report(request, scale):
+    field_add = {"scale_data.scale_id": scale}
+    result = dowellconnection("dowellscale", "bangalore", "dowellscale", "scale_reports", "scale_reports", "1094", "ABCDE",
+                        "fetch", field_add, "nil")
+    print(result, "result\n\n\n")
+    result = json.loads(result)
+    print(result['data'][0])
+    print(result['data'][0]['score']['score'])
+    scores = result['data'][0]['score']['score']
+    response_ = {}
 
-    return Response({"success": data_future.result()}, status=status.HTTP_200_OK)
+    # create random 9 digit process id
+    process_id = result['data'][0]['process_id']
+
+    normality, stattrics = statistics(scores, process_id)
+    print("\n\n\nnormality: ", normality)
+    print("\n\n\nstattrics: ", stattrics)
+    response_['result'] = result
+    response_["normality_analysis"] = normality
+    response_["central_tendencies"] = stattrics
+
+    return Response({"success": response_}, status=status.HTTP_200_OK)
 
 
 
