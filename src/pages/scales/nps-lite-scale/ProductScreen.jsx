864ec9e94846fff5dca4441708a075f0bde6fe2d
Pranai-1 <reddypranai2017@gmail.com>
2024-05-15 18:55:57 +0530

    fixed the screen according to size

--- src/pages/scales/nps-lite-scale/ProductScreen.jsx
+++ src/pages/scales/nps-lite-scale/ProductScreen.jsx
@@ -25,7 +25,7 @@ export default function ProductScreen({codeToCopy,buttonLinks}){
 
     return(
         <>
-        <div className="absolute top-[71%] right-[16%]  p-2">
+        <div className="absolute top-[71%] right-[16%]  p-2 flex flex-wrap">
         <BsThreeDotsVertical className="cursor-pointer" onClick={()=>setShowOptions((prev)=>!prev)}/>
     </div>
     {showOptions && (
@@ -38,27 +38,28 @@ export default function ProductScreen({codeToCopy,buttonLinks}){
         <button className="flex justify-center items-center gap-2 text-[12px]"><PiFileCsvDuotone/> Generate a .csv file</button>
         </div>
     )}
-     <div className="flex flex-col justify-center items-start font-normal mt-5">
-                    <p className=" p-2 mb-5">Copy the source code of your scale and integrate it on your website</p>
-                    <div className="h-[300px] w-[700px] bg-white overflow-auto p-2 text-[12px]">
+     <div className="flex flex-col justify-center items-start font-normal mt-5 flex-wrap w-[100%]">
+                    <p className=" p-2 mb-5 ">Copy the source code of your scale and integrate it on your website</p>
+                    <div className="h-[300px] w-[95%] bg-white overflow-auto p-2 text-[12px]">
                         <pre  style={{ fontFamily: 'Roboto, sans-serif' }} className="text-[14px]">
                        {codeToCopy}
                        </pre>
-                      <button className="flex justify-center text-[12px] text-white bg-[#606060] p-1 items-center gap-2 absolute top-[24%] right-[16%]"
+                      <button className="flex justify-center text-[12px] text-white bg-[#606060] p-1 items-center gap-2 absolute right-[18%] top-[33%] sm:right-[14%] sm:top-[29%] md:right-[12%] md:top-[28%] lg:right-[12%] lg:top-[26%] xl:top-[24%] xl:right-[10%]"
                       onClick={()=>{copyToClipboard(codeToCopy)
                       
                       }}> <MdContentCopy /> Copy code</button>
                   {isCopied && <p className="absolute top-[27%] text-[#00a3ff] right-[18%]">Copied!</p>}
                     </div>
                      <p className=" p-2 mt-5">Use the button links to add them to your scale</p>
-            <table className="w-full divide-y divide-gray-200 overflow-hidden mt-5 " >
-            <thead className="bg-gray-50">
+            <table className="w-[95%]  flex flex-col flex-wrap divide-y divide-gray-200 bg-gray-50 overflow-auto mt-5 
+            md:text-[12px] text-[8px]" >
+            <thead>
                 <tr>
-                    <th className="px-2 py-3 text-left text-[10px] font-medium text-black uppercase ">Scale Values</th>
-                    <th className="px-6 py-3 text-left text-[10px] font-medium text-black uppercase ">Button Links</th>
+                    <th className="px-2 py-3 text-left font-medium text-black uppercase ">Scale Values</th>
+                    <th className="px-6 py-3 text-left  font-medium text-black uppercase ">Button Links</th>
                 </tr>
             </thead>
-            <tbody className="bg-white divide-y divide-[#cfdfd8]">
+            <tbody className="bg-white divide-y divide-[#cfdfd8] w-max  ">
                 {buttonLinks.map((link, index) => (
                     <tr key={index} className="hover:bg-[#d5d5d5] hover:cursor-pointer" onMouseEnter={()=>setShowCopyIcon(index)}
                     onMouseLeave={()=>setShowCopyIcon(-1)}
@@ -66,10 +67,10 @@ export default function ProductScreen({codeToCopy,buttonLinks}){
                         setShowCopyIcon(-1)
                     }}
                    >
-                        <td className="px-6 py-2 text-[12px]">{ratings[index]}</td>
+                        <td className="px-6 py-2 ">{ratings[index]}</td>
                         <td className="px-6 py-2">
-                            <div className="overflow-hidden  max-w-[500px]">
-                                <div className=" text-[10px] truncate text-[#00a3ff]">{link}</div>
+                            <div className="overflow-hidden ">
+                                <div className="  text-[#00a3ff]">{link}</div>
                             </div>
                         </td>
                     </tr>
