c63e32dad4d5ffb3fae62278c86117f8955cd4ad
norahwinda <norahwinda48@gmail.com>
2024-06-03 22:38:10 +0300

    addition of query search params and nps scale page

--- src/pages/scales/report/Booth.jsx
+++ src/pages/scales/report/Booth.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
-import dowellLogo from '../../../assets/dowell-logo.png';
+import { useState, useEffect} from 'react';
+import { useSearchParams } from "react-router-dom";
 
 const Booth = () => {
+
+  const [boothInput, setBoothInput] = useState()
+  const [searchParams, setSearchParams] = useSearchParams({});
+  let currentUrlParams = new URLSearchParams(window.location.search);
+
+  useEffect(()=>{
+    setSearchParams({ scale_id: "665a8277d5d158ec8391aaf7" });
+  })
+
+  const handleGoButton = () =>{
+    console.log(boothInput)
+  }
   return (
     <div className='flex flex-col items-center mt-10 w-[320px] h-[550px] m-auto'>
       <img className='mt-10' src='https://cdn.discordapp.com/attachments/1108341894162952192/1247115439675277424/image.png?ex=665eda43&is=665d88c3&hm=f56e39dc3fdcc0568aaa30ee96283958693cb3909acc5dea18373633d5fc9f07&' alt='booth image'/>
       <h3 className='mt-[140px]'>Please enter your booth number</h3>
-      <input className='w-[150px] h-[30px] border-2 border-black mt-5' />
-      <button className='w-[70px] h-[30px] rounded-lg mt-[100px] bg-[#FEC39C]'>Go</button>
+      <input name='boothInput' value={boothInput} onChange={(e) =>setBoothInput(e.target.value)} type='number' className='w-[150px] h-[30px] border-2 border-black mt-5' />
+      <button className='w-[70px] h-[30px] rounded-lg mt-[100px] bg-[#FEC39C]'
+      onClick={handleGoButton}>Go</button>
     </div>
   )
 }
