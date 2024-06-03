c2fa6a28533a210b11b3f0b58b4fca70dddf2ac4
paolo <paolosiroko@gmail.com>
2024-06-03 17:34:50 +0300

    updated file structure

--- backend/npslite/urls.py
+++ backend/npslite/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import  npslite_home, dowell_npslite_scale, dowell_npslite_scale_settings, npslite_home_admin,brand_product_error, settings_api_view_create, submit_response_view, npslite_response_view, scale_response_api_view
 from .api_v4 import CreateNPSLiteScale, CreateNPSLiteResponse
-
+from .api_v5 import CreatedNPSLiteScale, CreatedNPSLiteResponse
 app_name = 'nps-lite'
 urlpatterns = [
     path('nps-lite-default/', npslite_home, name='default_page'),
@@ -15,6 +15,8 @@ urlpatterns = [
     path('api/nps-lite-all-responses', scale_response_api_view, name="npslite_all_responses"),
     path('api/v4/nps-lite-create-scale/', CreateNPSLiteScale.as_view(), name='nps-lite-create-scale'),
     path('api/v4/nps-lite-create-scale/<str:id>', CreateNPSLiteScale.as_view(), name='nps-lite-create-scale'),
-    path('api/v4/nps-lite-create-response/', CreateNPSLiteResponse.as_view(), name='nps-lite-create-response')
+    path('api/v4/nps-lite-create-response/', CreateNPSLiteResponse.as_view(), name='nps-lite-create-response'),
+    path('api/v5/nps-lite-create-scale/', CreatedNPSLiteScale.as_view(), name='nps-lite-created-scale'),
+    path('api/v5/nps-lite-create-response/', CreatedNPSLiteResponse.as_view(), name='nps-lite-created-response'),
 ]
 
