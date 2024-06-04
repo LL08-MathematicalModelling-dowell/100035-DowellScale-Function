8940a1896353bbb390a719fabca7bb6f48ca690b
norahwinda <norahwinda48@gmail.com>
2024-06-04 18:11:41 +0300

    The booth page

--- src/pages/scales/report/Booth.jsx
+++ src/pages/scales/report/Booth.jsx
@@ -21,7 +21,7 @@ const Booth = () => {
       <h3 className='mt-[140px]'>Please enter your booth number</h3>
       <input name='boothInput' value={boothInput} onChange={(e) =>setBoothInput(e.target.value)} type='number' className='w-[150px] h-[30px] border-2 border-black mt-5' />
       <button className='w-[70px] h-[30px] rounded-lg mt-[100px] bg-[#FEC39C]'
-      onClick={handleGoButton}><Link to="http://127.0.0.1:8000/nps-lite/api/v5/nps-lite-create-scale/?user=True&scale_type=nps_lite&workspace_id=65b8d6f13d801280ffe56d65&username=Paolo&scale_id=665ed61fa746caf0d6d407c9&channel_name=channel_1">Go</Link></button>
+      onClick={handleGoButton}><Link to={`https://100035.pythonanywhere.com/nps-lite/api/v5/nps-lite-create-scale/?user=False&scale_type=nps_lite&workspace_id=1499&username=HeenaK&scale_id=665f0983e6c29a9077f3ef10&channel_name=channel_1&instance_id=${boothInput}`}>Go</Link></button>
     </div>
   )
 }
