e33448d1ddea5582bcc7ea2956e7c56c721d4c0c
norahwinda <norahwinda48@gmail.com>
2024-06-05 10:30:16 +0300

    Scale page

--- src/pages/scales/report/Booth.jsx
+++ src/pages/scales/report/Booth.jsx
@@ -26,7 +26,7 @@ const Booth = () => {
   }
   return (
     <div className='flex flex-col items-center mt-10 w-[320px] h-[550px] m-auto'>
-      <img className='mt-10' src='https://cdn.discordapp.com/attachments/1108341894162952192/1247115439675277424/image.png?ex=665eda43&is=665d88c3&hm=f56e39dc3fdcc0568aaa30ee96283958693cb3909acc5dea18373633d5fc9f07&' alt='booth image'/>
+      <img className='mt-10' src='https://i0.wp.com/dowellresearch.de/wp-content/uploads/2023/04/true-moments-logo-1-1-442919954-1538479590775-1.webp?w=382&ssl=1' alt='booth image'/>
       <h3 className='mt-[140px]'>Please enter your booth number</h3>
       <input name='boothInput' value={boothInput} onChange={(e) =>setBoothInput(e.target.value)} type='number' className='w-[150px] h-[30px] border-2 border-black mt-5' />
       <button className='w-[70px] h-[30px] rounded-lg mt-[100px] bg-[#FEC39C]'
