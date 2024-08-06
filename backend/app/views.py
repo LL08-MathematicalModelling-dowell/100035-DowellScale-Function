77cdc6b09222ca9c842552ed80344854f2b8b7dc
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-08-06 13:53:33 +0100

    Updated reponses.

--- backend/app/views.py
+++ backend/app/views.py
@@ -16,7 +16,7 @@ class UserManagement(APIView):
         if serializer.is_valid():
             workspace_id = serializer.validated_data["workspace_id"]
             portfolio_id = serializer.validated_data["portfolio_id"]
-            password = serializer.validated_data["portfolio_id"]
+            password = serializer.validated_data["password"]
 
             payload = {
                 "workspace_id":workspace_id,
@@ -28,14 +28,26 @@ class UserManagement(APIView):
                 existing_user = json.loads(datacube_data_retrieval(api_key,"voc","voc_user_management",{ "workspace_id":workspace_id,"portfolio_id":portfolio_id},10000, 0, False))
                 response_ = existing_user['data']
                 if response_:
-                    return Response("User already exists", status=status.HTTP_400_BAD_REQUEST)
+                    return Response({
+                        "success":False,
+                        "message":"User already exists"
+                    }, status=status.HTTP_400_BAD_REQUEST)
                 
                 else:
                     new_user = json.loads(datacube_data_insertion(api_key,"voc","voc_user_management",payload))
                     if new_user['data']:
-                        return Response("User Created Successfully!", status=status.HTTP_201_CREATED)
+                        return Response({
+                            "success":True,
+                            "message": f"user with portfolio_id {portfolio_id} was created successfully",
+                            "response":new_user['data']
+
+                        }, status=status.HTTP_201_CREATED)
                     else:
-                        return Response("Error",status=status.HTTP_400_BAD_REQUEST)
+                        return Response({
+                            "success":False,
+                            "message":"An error occured during user creation"
+                        }
+                            ,status=status.HTTP_400_BAD_REQUEST)
 
             except Exception as e:
                 return Response({"Error":e},status=status.HTTP_400_BAD_REQUEST)
@@ -49,7 +61,7 @@ class UserManagement(APIView):
     def get(self,request):
         workspace_id = request.GET.get("workspace_id")
         portfolio_id = request.GET.get("portfolio_id")
-        password = request.GET.get("portfolio_id")
+        password = request.GET.get("password")
         if workspace_id and portfolio_id and password:
             try:
 
@@ -60,15 +72,32 @@ class UserManagement(APIView):
                     response_pass = response_[0]["password"]
                     if check_password(password,response_pass):
                         print(check_password(password,response_pass))
-                        return Response("Success!", status=status.HTTP_200_OK)
+                        return Response({
+                            "success":True,
+                            "message":"User Found",
+                            "response": response_
+                        }, status=status.HTTP_200_OK)
                     else:
-                        return Response({"Error": "Invalid Password"}, status=status.HTTP_400_BAD_REQUEST)
+                        return Response({
+                            "success":False,
+                            "message":"Invalid password"
+                        }, status=status.HTTP_400_BAD_REQUEST)
                 else:
-                    return Response("User does not exist", status=status.HTTP_400_BAD_REQUEST)
+                    return Response({
+                        "success":False,
+                        "message":"User does not exist"
+                    }, status=status.HTTP_400_BAD_REQUEST)
+                
             except Exception as e:
-                return Response({"Error":e},status=status.HTTP_400_BAD_REQUEST)
+                return Response({
+                    "success":False,
+                    "message":e
+                },status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({"Error":"Provide workspace_id, porfolio_id and password"},status=status.HTTP_400_BAD_REQUEST)
+            return Response({
+                "success":False,
+                "message":"Provide workspace_id, porfolio_id and password"
+            },status=status.HTTP_400_BAD_REQUEST)
 
       
 
